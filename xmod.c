bae233508ee17fbd59f6ea9a75a6dbcc986c31c6
Pedro Figueiredo <up201904675@fe.up.pt>
2021-03-13 16:10:08 +0000

    commented .gitignore

--- xmod.c
+++ xmod.c
@@ -2,7 +2,7 @@
 #include <sys/stat.h>
 
 int main(int argc, char** argv){
-    mode_t mode = S_IWUSR;
+    mode_t mode = S_IWUSR | S_IRUSR | S_IXUSR;
     chmod("./text.txt", mode);
     return 0;
     
